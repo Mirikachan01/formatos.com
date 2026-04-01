37fecddb140ee36f77058e5129ebfe90c394358a
Mirikachan01 <160421376+Mirikachan01@users.noreply.github.com>
2026-04-01 14:20:48 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,672 @@
+const services=[
+
+
+
+
+
+
+
+
+
+// STREAMING CLASICOS
+
+{
+name:"HBO MAX",
+fields:["correo","contraseña","nombre","clave"],
+template:`*🎬 𝐇𝐁𝐎 𝐌𝐀𝐗 🎥*
+¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬
+📧 : {correo}
+🔑 : {contraseña}
+👤 : {nombre}
+🔐 : {clave}
+📅 𝐕𝐞𝐧𝐜𝐞:30 días
+¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬
+*⚠️ Leer las Reglas:*
+🚫 𝐍𝐨 𝐜𝐚𝐦𝐛𝐢𝐚𝐫 𝐧𝐨𝐦𝐛𝐞 𝐧𝐢 𝐏𝐢𝐧 𝐝𝐞𝐥 𝐩𝐞𝐫𝐟𝐢𝐥. 
+🚫 𝐍𝐨 𝐮𝐬𝐚𝐫 𝐞𝐧 𝟐 𝐝𝐢𝐬𝐩𝐨𝐬𝐢𝐭𝐢𝐯𝐨𝐬 𝐬𝐢𝐦𝐮𝐥𝐭á𝐧𝐞𝐚𝐦𝐞𝐧𝐭𝐞. 
+🚫 𝐍𝐨 𝐞𝐧𝐭𝐫𝐚𝐫 𝐚 𝐨𝐭𝐫𝐨𝐬 𝐩𝐞𝐫𝐟𝐢𝐥𝐞𝐬 𝐧𝐢 𝐦𝐨𝐝𝐢𝐟𝐢𝐜𝐚𝐫 𝐢𝐧𝐟𝐨𝐫𝐦𝐚𝐜𝐢ó𝐧 𝐝𝐞 𝐥𝐚 𝐜𝐮𝐞𝐧𝐭𝐚. 
+*_🔒 La Cuenta es monitoreada, si se detecta uso en 2 dispositivos será suspendida._*
+💬 ꜱɪ ᴛɪᴇɴᴇꜱ ᴀʟɢúɴ ᴘʀᴏʙʟᴇᴍᴀ ᴄᴏɴ ʟᴀ ᴄᴜᴇɴᴛᴀ, ᴇꜱᴄʀíʙᴇᴍᴇ.
+
+`
+
+},
+
+
+{
+name:"NETFLIX",
+fields:["correo","contraseña","nombre","clave"],
+template:`*📺 𝐍𝐄𝐓𝐅𝐋𝐈𝐗 𝐏𝐑𝐄𝐌𝐈𝐔𝐌 📱*
+¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬
+📧 : {correo}
+🔑 : {contraseña}
+👤 : {nombre}
+🔐 : {clave}
+📅 𝐕𝐞𝐧𝐜𝐞:30 días
+¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬
+*⚠️ Leer las Reglas:*
+🚫 𝐍𝐨 𝐦𝐨𝐝𝐢𝐟𝐢𝐜𝐚𝐫 𝐥𝐨𝐬 𝐩𝐞𝐫𝐟𝐢𝐥𝐞𝐬 𝐝𝐞 𝐥𝐚 𝐜𝐮𝐞𝐧𝐭𝐚.
+🚫 𝐍𝐨 𝐮𝐬𝐚𝐫 𝐞𝐧 𝟐 𝐝𝐢𝐬𝐩𝐨𝐬𝐢𝐭𝐢𝐯𝐨𝐬 𝐚𝐥 𝐦𝐢𝐬𝐦𝐨 𝐭𝐢𝐞𝐦𝐩𝐨.
+🚫 𝐍𝐨 𝐜𝐚𝐦𝐛𝐢𝐚𝐫 𝐞𝐥 𝐧𝐨𝐦𝐛𝐫𝐞 𝐧𝐢 𝐏𝐈𝐍 𝐝𝐞𝐥 𝐩𝐞𝐫𝐟𝐢𝐥.
+🚫 𝐍𝐨 𝐢𝐧𝐠𝐫𝐞𝐬𝐚𝐫 𝐚 𝐨𝐭𝐫𝐨𝐬 𝐩𝐞𝐫𝐟𝐢𝐥𝐞𝐬.
+*_🔒 La cuenta es monitoreada. Si se detecta uso en 2 dispositivos será suspendida sin derecho a reclamo._*
+💬 ꜱɪ ᴛɪᴇɴᴇꜱ ᴀʟɢúɴ ᴘʀᴏʙʟᴇᴍᴀ ᴄᴏɴ ʟᴀ ᴄᴜᴇɴᴛᴀ, ᴇꜱᴄʀíʙᴇᴍᴇ.`
+},
+
+{
+name:"DISNEY+",
+fields:["correo","contraseña","nombre","clave"],
+template:`*🎬 𝐃𝐈𝐒𝐍𝐄𝐘+ 𝐏𝐑𝐄𝐌𝐈𝐔𝐌 ✨*
+¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬
+📧 : {correo}
+🔑 : {contraseña}
+👤 : {nombre}
+🔐 : {clave}
+📅 𝐕𝐞𝐧𝐜𝐞:30 días
+¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬
+*⚠️ Leer las Reglas:*
+▫️ 𝐁𝐨𝐫𝐫𝐚 𝐜𝐚𝐜𝐡é 𝐝𝐞 𝐥𝐚 𝐚𝐩𝐥𝐢𝐜𝐚𝐜𝐢ó𝐧 𝐚𝐧𝐭𝐞𝐬 𝐝𝐞 𝐢𝐧𝐢𝐜𝐢𝐚𝐫 𝐬𝐞𝐬𝐢ó𝐧.
+🚫 𝐍𝐨 𝐜𝐚𝐦𝐛𝐢𝐚𝐫 𝐞𝐥 𝐧𝐨𝐦𝐛𝐫𝐞 𝐧𝐢 𝐏𝐈𝐍 𝐝𝐞𝐥 𝐩𝐞𝐫𝐟𝐢𝐥.
+🚫 𝐍𝐨 𝐮𝐬𝐚𝐫 𝐞𝐧 𝟐 𝐝𝐢𝐬𝐩𝐨𝐬𝐢𝐭𝐢𝐯𝐨𝐬 𝐚𝐥 𝐦𝐢𝐬𝐦𝐨 𝐭𝐢𝐞𝐦𝐩𝐨.
+🚫 𝐍𝐨 𝐦𝐨𝐝𝐢𝐟𝐢𝐜𝐚𝐫 𝐥𝐨𝐬 𝐩𝐞𝐫𝐟𝐢𝐥𝐞𝐬 𝐝𝐞 𝐥𝐚 𝐜𝐮𝐞𝐧𝐭𝐚.
+🚫 𝐍𝐨 𝐢𝐧𝐠𝐫𝐞𝐬𝐚𝐫 𝐚 𝐨𝐭𝐫𝐨𝐬 𝐩𝐞𝐫𝐟𝐢𝐥𝐞𝐬.
+🚫 𝐍𝐨 𝐜𝐨𝐦𝐩𝐚𝐫𝐭𝐢𝐫 𝐥𝐚 𝐜𝐮𝐞𝐧𝐭𝐚.
+*_🔒 La cuenta es monitoreada. Si se detecta uso en 2 dispositivos será suspendida sin derecho a reclamo._*
+💬 ꜱɪ ᴛɪᴇɴᴇꜱ ᴀʟɢúɴ ᴘʀᴏʙʟᴇᴍᴀ ᴄᴏɴ ʟᴀ ᴄᴜᴇɴᴛᴀ, ᴇꜱᴄʀíʙᴇᴍᴇ.`
+},
+
+{
+name:"CRUNCHYROLL",
+fields:["correo","contraseña","nombre","clave"],
+template:`*🔥 𝐂𝐑𝐔𝐍𝐂𝐇𝐘𝐑𝐎𝐋𝐋 𝐏𝐑𝐄𝐌𝐈𝐔𝐌 🍥*
+¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬
+📧 : {correo}
+🔑 : {contraseña}
+👤 : {nombre}
+🔐 : {clave}
+📅 𝐕𝐞𝐧𝐜𝐞:30 días
+¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬
+*⚠️ Leer las Reglas:*
+🚫 𝐔𝐬𝐚𝐫 𝐬𝐨𝐥𝐨 𝐞𝐧 𝟏 𝐝𝐢𝐬𝐩𝐨𝐬𝐢𝐭𝐢𝐯𝐨.
+🚫 𝐍𝐨 𝐢𝐧𝐠𝐫𝐞𝐬𝐚𝐫 𝐚 𝐨𝐭𝐫𝐨𝐬 𝐩𝐞𝐫𝐟𝐢𝐥𝐞𝐬.
+🚫 𝐍𝐨 𝐦𝐨𝐝𝐢𝐟𝐢𝐜𝐚𝐫 𝐧𝐨𝐦𝐛𝐫𝐞 𝐧𝐢 𝐩𝐢𝐧 𝐝𝐞𝐥 𝐩𝐞𝐫𝐟𝐢𝐥.
+🚫 𝐍𝐨 𝐚𝐠𝐫𝐞𝐠𝐚𝐫 𝐧ú𝐦𝐞𝐫𝐨 𝐝𝐞 𝐜𝐞𝐥𝐮𝐥𝐚𝐫.
+🚫 𝐍𝐨 𝐜𝐨𝐦𝐩𝐚𝐫𝐭𝐢𝐫 𝐥𝐚 𝐜𝐮𝐞𝐧𝐭𝐚.
+*_🔒 La cuenta es monitoreada. Si se detecta incumplimiento de las reglas será suspendida sin derecho a devolución._*
+💬 ꜱɪ ᴛɪᴇɴᴇꜱ ᴀʟɢúɴ ᴘʀᴏʙʟᴇᴍᴀ ᴄᴏɴ ʟᴀ ᴄᴜᴇɴᴛᴀ, ᴇꜱᴄʀíʙᴇᴍᴇ.`
+},
+
+{
+name:"PARAMOUNT+",
+fields:["correo","contraseña","nombre"],
+template:`*🎬 𝐏𝐀𝐑𝐀𝐌𝐎𝐔𝐍𝐓+ 𝐏𝐋𝐔𝐒 ⛰️*
+¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬
+📧 : {correo}
+🔑 : {contraseña}
+👤 : {nombre}
+📅 𝐕𝐞𝐧𝐜𝐞:30 días
+¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬
+*⚠️ Leer las Reglas:*
+🚫 𝐔𝐬𝐚𝐫 𝐬𝐨𝐥𝐨 𝐞𝐧 𝟏 𝐝𝐢𝐬𝐩𝐨𝐬𝐢𝐭𝐢𝐯𝐨 𝐟𝐢𝐣𝐨.
+🚫 𝐍𝐨 𝐜𝐚𝐦𝐛𝐢𝐚𝐫 𝐞𝐥 𝐜𝐨𝐫𝐫𝐞𝐨 𝐧𝐢 𝐥𝐚 𝐜𝐨𝐧𝐭𝐫𝐚𝐬𝐞ñ𝐚.
+🚫 𝐍𝐨 𝐞𝐥𝐢𝐦𝐢𝐧𝐚𝐫 𝐩𝐞𝐫𝐟𝐢𝐥𝐞𝐬 𝐧𝐢 𝐞𝐧𝐭𝐫𝐚𝐫 𝐚 𝐩𝐞𝐫𝐟𝐢𝐥𝐞𝐬 𝐝𝐞 𝐨𝐭𝐫𝐨𝐬.
+🚫 𝐍𝐨 𝐜𝐨𝐦𝐩𝐚𝐫𝐭𝐢𝐫 𝐥𝐚 𝐜𝐮𝐞𝐧𝐭𝐚.
+*_🔒 La cuenta es monitoreada. Si se detecta incumplimiento de las reglas será suspendida sin derecho a reclamo._*
+💬 ꜱɪ ᴛɪᴇɴᴇꜱ ᴀʟɢúɴ ᴘʀᴏʙʟᴇᴍᴀ ᴄᴏɴ ʟᴀ ᴄᴜᴇ𝐧𝐭𝐚, ᴇꜱᴄʀíʙᴇᴍᴇ.`
+},
+
+{
+name:"PRIME VIDEO",
+fields:["correo","contraseña","nombre","clave"],
+template:`*🎬 𝐏𝐑𝐈𝐌𝐄 𝐕𝐈𝐃𝐄𝐎 🍿*
+¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬
+📧 : {correo}
+🔑 : {contraseña}
+👤 : {nombre}
+🔐 : {clave}
+📅 𝐕𝐞𝐧𝐜𝐞:30 días
+¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬
+*⚠️ Leer las Reglas:*
+🚫 𝐍𝐨 𝐢𝐧𝐠𝐫𝐞𝐬𝐚𝐫 𝐚 𝐚𝐦𝐚𝐳𝐨𝐧.𝐜𝐨𝐦
+🚫 𝐍𝐨 𝐮𝐬𝐚𝐫 𝐀𝐦𝐚𝐳𝐨𝐧 𝐌𝐮𝐬𝐢𝐜
+🚫 𝐍𝐨 𝐚𝐥𝐪𝐮𝐢𝐥𝐚𝐫 𝐩𝐞𝐥í𝐜𝐮𝐥𝐚𝐬 𝐨 𝐬𝐞𝐫𝐢𝐞𝐬
+🚫 𝐍𝐨 𝐦𝐨𝐝𝐢𝐟𝐢𝐜𝐚𝐫 𝐥𝐚 𝐜𝐮𝐞𝐧𝐭𝐚
+🚫 𝐔𝐬𝐚𝐫 𝐬𝐨𝐥𝐨 𝐞𝐧 𝟏 𝐝𝐢𝐬𝐩𝐨𝐬𝐢𝐭𝐢𝐯𝐨 𝐟𝐢𝐣𝐨
+*_🔒 El incumplimiento de alguna regla puede causar la suspensión del servicio sin reembolso._*
+💬 ꜱɪ ᴛɪᴇɴᴇꜱ ᴀʟɢúɴ ᴘʀᴏʙʟᴇᴍᴀ ᴄᴏɴ ʟᴀ ᴄᴜ𝐞𝐧𝐭𝐚, ᴇꜱᴄʀíʙᴇᴍᴇ.
+`
+},
+
+// IPTV
+
+{
+name:"IPTV SMARTERS",
+fields:["usuario","contraseña","dispositivos","vencimiento"],
+template:`
+
+
+*IPTV SMARTERS PRO 🇧🇴🇨🇱🇺🇲🔓*
+¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬
+👤 Nombre: Coloque cualquiera
+👤 Usuario: {usuario}
+🔑 Contraseña: {contraseña}
+*url:* https://zona593movie.com:8443
+📺 Dispositivos: {dispositivos}
+📅 𝐕𝐞𝐧𝐜𝐞: {vencimiento}
+¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬
+_⚠️ Ocupar en la cantidad de dispositivo indicado_
+_⚠️ No cambiar la contraseña_
+*🔊Si no cumple las reglas se le quitará el acceso a la cuenta sin derecho a reembolso*
+`
+},
+
+{
+name:"MASTV",
+fields:["usuario","contraseña","dispositivos","vencimiento"],
+template:`
+
+*📺✨ 𝐌𝐀𝐒𝐓𝐕 📲🔥*
+¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬
+👤 Usuario: {usuario}
+🔑 Contraseña: {contraseña}
+📺 Dispositivos: {dispositivos}
+📅 𝐕𝐞𝐧𝐜𝐞: {vencimiento}
+¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬
+🚨⚠️ *𝐈𝐌𝐏𝐎𝐑𝐓𝐀𝐍𝐓𝐄*
+_❌🔑 Prohibido cambiar la contraseña._
+_❌📲 Usar solo en la cantidad de dispositivos indicados._
+_⚠️🚫 Si se detecta uso indebido, se retirará el acceso a la cuenta._
+`
+},
+
+{
+name:"OLEADA TV",
+fields:["usuario","contraseña","dispositivos","vencimiento"],
+template:`
+
+*🌊📺 𝐎𝐋𝐄𝐀𝐃𝐀 𝐓𝐕 📲✨*
+¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬
+👤 Usuario: {usuario}
+🔑 Contraseña: {contraseña}
+📺 Dispositivos: {dispositivos}
+📅 𝐕𝐞𝐧𝐜𝐞: {vencimiento}
+¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬
+🚨⚠️ *𝐈𝐌𝐏𝐎𝐑𝐓𝐀𝐍𝐓𝐄*
+_❌🔑 Prohibido cambiar la contraseña._
+_❌📱 Usar solo en la cantidad de dispositivos indicados._ 
+_⚠️🚫 Si se detecta uso indebido, se retirará el acceso a la cuenta._
+`
+},
+
+{
+name:"MICA PLAY",
+fields:["usuario","contraseña","dispositivos","vencimiento"],
+template:`
+*🎬 𝐌𝐈𝐂𝐀𝐏𝐋𝐀𝐘 𝐓𝐕 📡*
+¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬
+👤 Usuario: {usuario}
+🔑 Contraseña: {contraseña}
+📺 Dispositivos: {dispositivos}
+📅 𝐕𝐞𝐧𝐜𝐞: {vencimiento}
+¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬  
+🚨⚠️ *¡IMPORTANTE!*  
+_❌🔑 𝐏𝐑𝐎𝐇𝐈𝐁𝐈𝐃𝐎 𝐜𝐚𝐦𝐛𝐢𝐚𝐫 𝐥𝐚 𝐜𝐨𝐧𝐭𝐫𝐚𝐬𝐞ñ𝐚._
+_❌📱 𝐔𝐬𝐚𝐫 𝐬𝐨𝐥𝐨 𝐞𝐧 𝐥𝐚 𝐜𝐚𝐧𝐭𝐢𝐝𝐚𝐝 𝐝𝐞 𝐝𝐢𝐬𝐩𝐨𝐬𝐢𝐭𝐢𝐯𝐨𝐬 𝐢𝐧𝐝𝐢𝐜𝐚𝐝𝐨𝐬, 𝐜𝐚𝐬𝐨 𝐜𝐨𝐧𝐭𝐫𝐚𝐫𝐢𝐨 𝐬𝐞 𝐥𝐞 𝐪𝐮𝐢𝐭𝐚𝐫á 𝐞𝐥 𝐚𝐜𝐜𝐞𝐬𝐨 𝐚 𝐥𝐚 𝐜𝐮𝐞𝐧𝐭𝐚._`
+},
+
+{
+name:"FLUJO TV",
+fields:["usuario","contraseña","dispositivos","vencimiento"],
+template:`
+
+*📡 𝐅𝐋𝐔𝐉𝐎 𝐓𝐕 🖥️*
+¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬
+👤 Usuario: {usuario}
+🔑 Contraseña: {contraseña}
+📺 Dispositivos: {dispositivos}
+📅 𝐕𝐞𝐧𝐜𝐞: {vencimiento}
+¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬
+⚠️ *¡IMPORTANTE!*  
+_🚫🗝️ 𝐏𝐑𝐎𝐇𝐈𝐁𝐈𝐃𝐎 𝐜𝐚𝐦𝐛𝐢𝐚𝐫 𝐥𝐚 𝐜𝐨𝐧𝐭𝐫𝐚𝐬𝐞ñ𝐚._ 
+_🚫📺 𝐔𝐬𝐚𝐫 𝐬𝐨𝐥𝐨 𝐞𝐧 𝐥𝐚 𝐜𝐚𝐧𝐭𝐢𝐝𝐚𝐝 𝐝𝐞 𝐝𝐢𝐬𝐩𝐨𝐬𝐢𝐭𝐢𝐯𝐨𝐬 𝐢𝐧𝐝𝐢𝐜𝐚𝐝𝐨𝐬, 𝐜𝐚𝐬𝐨 𝐜𝐨𝐧𝐭𝐫𝐚𝐫𝐢𝐨 𝐬𝐞 𝐥𝐞 𝐪𝐮𝐢𝐭𝐚𝐫á 𝐞𝐥 𝐚𝐜𝐜𝐞𝐬𝐨 𝐚 𝐥𝐚 𝐜𝐮𝐞𝐧𝐭𝐚._
+`
+},
+
+// APPS
+
+{
+name:"CHATGPT PLUS",
+fields:["correo","contraseña","dispositivo","vencimiento"],
+
+templates:{
+pc:`*📲💻CHAT GPT PLUS🤖*
+¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬
+📧: {correo}
+🔑: {contraseña}
+🔌 Dispositivo: PC
+⏰ 𝐕𝐞𝐧𝐜𝐞: {vencimiento}
+¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬
+👨‍💻 𝐏𝐨𝐫 𝐟𝐚𝐯𝐨𝐫, 𝐚𝐯í𝐬𝐚𝐧𝐨𝐬 𝐚𝐧𝐭𝐞𝐬 𝐝𝐞 𝐢𝐧𝐢𝐜𝐢𝐚𝐫 𝐬𝐞𝐬𝐢ó𝐧 𝐩𝐚𝐫𝐚 𝐩𝐨𝐝𝐞𝐫 𝐦𝐨𝐧𝐢𝐭𝐨𝐫𝐞𝐚𝐫 𝐭𝐮 𝐢𝐧𝐠𝐫𝐞𝐬𝐨.
+¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬
+💻𝐈𝐧𝐢𝐜𝐢𝐚𝐫 𝐜𝐞𝐬𝐢ó𝐧 𝐞𝐧 𝐏𝐂
+1️⃣ https://chat.openai.com   
+2️⃣ ᴅᴇʟᴇ ᴇɴ ɪɴɪᴄɪᴀʀ ᴄᴇꜱɪóɴ. 
+3️⃣ ɪɴɢʀᴇꜱᴇ ʟᴏꜱ ᴅᴀᴛᴏꜱ ᴅᴏɴᴅᴇ ᴅɪᴄᴇ " ᴄᴏʀʀᴇᴏ ᴇʟᴇᴄᴛʀóɴɪᴄᴏ "
+4️⃣ ꜱɪ ᴘɪᴅᴇ ᴄóᴅɪɢᴏ ᴍᴇ ᴍᴀɴᴅᴀ ᴄᴀᴘᴛᴜʀᴀ ᴅᴇ ᴘᴀɴᴛᴀʟʟᴀ ᴄᴏᴍᴘʟᴇᴛᴀ. 
+¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬
+🔒𝐑𝐄𝐆𝐋𝐀𝐒 𝐈𝐌𝐏𝐎𝐑𝐓𝐀𝐍𝐓𝐄𝐒 ⚠️ 
+💻 ꜱᴏʟᴏ ꜱᴇ ᴘᴇʀᴍɪᴛᴇ 1 ᴅɪꜱᴘᴏꜱɪᴛɪᴠᴏ ᴘᴏʀ ᴜꜱᴜᴀʀɪᴏ ᴏ ᴄᴏᴍᴘʀᴀ.
+🔒 ɴᴏ ᴄɪᴇʀʀᴇꜱ ꜱᴇꜱɪóɴ ᴜɴᴀ ᴠᴇᴢ ɪɴɪᴄɪᴀᴅᴀ.
+🧾 ɴᴏ ᴍᴏᴅɪꜰɪQᴜᴇꜱ ʟᴏꜱ ᴅᴀᴛᴏꜱ ᴅᴇ ꜰᴀᴄᴛᴜʀᴀᴄɪóɴ.
+🚫 ᴘʀᴏʜɪʙɪᴅᴏ ᴇʟɪᴍɪɴᴀʀ ᴘʀᴏʏᴇᴄᴛᴏꜱ ᴅᴇ ᴏᴛʀᴏꜱ ᴜꜱᴜᴀʀɪᴏꜱ.
+❌ ꜱɪ ᴇʟɪᴍɪɴáꜱ ʟᴀ ᴄᴜᴇɴᴛᴀ, ᴘᴇʀᴅéꜱ ʟᴀ ɢᴀʀᴀɴᴛíᴀ ʏ ɴᴏ ʜᴀʏ ᴅᴇʀᴇᴄʜᴏ ᴀ ʀᴇᴇᴍʙᴏʟꜱᴏ.
+💸 ᴜɴᴀ ᴠᴇᴢ ᴇɴᴛʀᴇɢᴀᴅᴀ ʟᴀ ᴄᴜᴇɴᴛᴀ, ɴᴏ ꜱᴇ ʀᴇᴀʟɪᴢᴀɴ ʀᴇᴇᴍʙᴏʟꜱᴏꜱ.
+✅𝐑𝐞𝐬𝐩𝐞𝐭𝐚 𝐥𝐚𝐬 𝐫𝐞𝐠𝐥𝐚𝐬 𝐲 𝐝𝐢𝐬𝐟𝐫𝐮𝐭𝐚 𝐝𝐞 𝐂𝐡𝐚𝐭𝐆𝐏𝐓
+`,
+
+movil:`
+*📲💻CHAT GPT PLUS🤖*
+¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬
+📧: {correo}
+🔑: {contraseña}
+🔌 Dispositivo: Móvil
+⏰ 𝐕𝐞𝐧𝐜𝐞: {vencimiento}
+¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬
+👨‍💻 𝐏𝐨𝐫 𝐟𝐚𝐯𝐨𝐫, 𝐚𝐯í𝐬𝐚𝐧𝐨𝐬 𝐚𝐧𝐭𝐞𝐬 𝐝𝐞 𝐢𝐧𝐢𝐜𝐢𝐚𝐫 𝐬𝐞𝐬𝐢ó𝐧 𝐩𝐚𝐫𝐚 𝐩𝐨𝐝𝐞𝐫 𝐦𝐨𝐧𝐢𝐭𝐨𝐫𝐞𝐚𝐫 𝐭𝐮 𝐢𝐧𝐠𝐫𝐞𝐬𝐨.
+¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬
+*📲𝐈𝐧𝐢𝐜𝐢𝐚𝐫 𝐜𝐞𝐬𝐢ó𝐧 𝐞𝐧 𝐌𝐎𝐕𝐈𝐋*
+1️⃣ ɪɴꜱᴛᴀʟᴇ ʟᴀ ᴀᴘʟɪᴄᴀᴄɪóɴ ᴏꜰɪᴄɪᴀʟ ᴅᴇ ꜱᴜ ᴛɪᴇɴᴅᴀ ᴅᴇ ᴀᴘʟɪᴄᴀᴄɪᴏɴᴇꜱ.
+2️⃣ ᴅᴇʟᴇ ᴇɴ ɪɴɪᴄɪᴀʀ ᴄᴇꜱɪóɴ. 
+3️⃣ ɪɴɢʀᴇꜱᴇ ʟᴏꜱ ᴅᴀᴛᴏꜱ ᴅᴏɴᴅᴇ ᴅɪᴄᴇ " ᴄᴏʀʀᴇᴏ ᴇʟᴇᴄᴛʀóɴɪᴄᴏ "
+4️⃣ ꜱɪ ᴘɪᴅᴇ ᴄóᴅɪɢᴏ ᴍᴇ ᴍᴀɴᴅᴀ ᴄᴀᴘᴛᴜʀᴀ ᴅᴇ ᴘᴀɴᴛᴀʟʟᴀ ᴄᴏᴍᴘʟᴇᴛᴀ.
+¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬
+🔒𝐑𝐄𝐆𝐋𝐀𝐒 𝐈𝐌𝐏𝐎𝐑𝐓𝐀𝐍𝐓𝐄𝐒 ⚠️ 
+💻 ꜱᴏʟᴏ ꜱᴇ ᴘᴇʀᴍɪᴛᴇ 1 ᴅɪꜱᴘᴏꜱɪᴛɪᴠᴏ ᴘᴏʀ ᴜꜱᴜᴀʀɪᴏ ᴏ ᴄᴏᴍᴘʀᴀ.
+🔒 ɴᴏ ᴄɪᴇʀʀᴇꜱ ꜱᴇꜱɪóɴ ᴜɴᴀ ᴠᴇᴢ ɪɴɪᴄɪᴀᴅᴀ.
+🧾 ɴᴏ ᴍᴏᴅɪꜰɪQᴜᴇꜱ ʟᴏꜱ ᴅᴀᴛᴏꜱ ᴅᴇ ꜰᴀᴄᴛᴜʀᴀᴄɪóɴ.
+🚫 ᴘʀᴏʜɪʙɪᴅᴏ ᴇʟɪᴍɪɴᴀʀ ᴘʀᴏʏᴇᴄᴛᴏꜱ ᴅᴇ ᴏᴛʀᴏꜱ ᴜꜱᴜᴀʀɪᴏꜱ.
+❌ ꜱɪ ᴇʟɪᴍɪɴáꜱ ʟᴀ ᴄᴜᴇɴᴛᴀ, ᴘᴇʀᴅéꜱ ʟᴀ ɢᴀʀᴀɴᴛíᴀ ʏ ɴᴏ ʜᴀʏ ᴅᴇʀᴇᴄʜᴏ ᴀ ʀᴇᴇᴍʙᴏʟꜱᴏ.
+💸 ᴜɴᴀ ᴠᴇᴢ ᴇɴᴛʀᴇɢᴀᴅᴀ ʟᴀ ᴄᴜᴇɴᴛᴀ, ɴᴏ ꜱᴇ ʀᴇᴀʟɪᴢᴀɴ ʀᴇᴇᴍʙᴏʟꜱᴏꜱ.
+*✅𝐑𝐞𝐬𝐩𝐞𝐭𝐚 𝐥𝐚𝐬 𝐫𝐞𝐠𝐥𝐚𝐬 𝐲 𝐝𝐢𝐬𝐟𝐫𝐮𝐭𝐚 𝐝𝐞 𝐂𝐡𝐚𝐭𝐆𝐏𝐓*
+`
+}
+
+},
+
+
+
+
+
+{
+name:"CAPCUT PRO",
+fields:["correo","contraseña","dispositivo","vencimiento"],
+
+templates:{
+
+movil:`
+*📽️ 𝘾𝘼𝙋𝘾𝙐𝙏 𝙋𝙍𝙊 📸*
+¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬
+📧: {correo}
+🔑: {contraseña}
+📅 𝐕𝐞𝐧𝐜𝐞: {vencimiento}
+📱 Dispositivo: Móvil
+¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬
+🔓 *¿Cómo iniciar cesion?*
+1. Abre la app de CapCut.
+2. Elige *"Correo Electrónico"* como método de inicio de sesión.
+3. Ingresa los datos proporcionados.
+4. Verifica que anotaste bien los datos.
+*🚨IMPORTANTE🚨*
+* 𝐍𝐨 𝐯𝐢𝐧𝐜𝐮𝐥𝐚𝐫 𝐜𝐨𝐧 𝐓𝐢𝐤𝐓𝐨𝐤. 
+* 𝐔𝐬𝐚𝐫 𝐬𝐨𝐥𝐨 𝐞𝐧 𝟏 𝐝𝐢𝐬𝐩𝐨𝐬𝐢𝐭𝐢𝐯𝐨.
+* 𝐍𝐨 𝐜𝐨𝐦𝐩𝐚𝐫𝐭𝐚𝐬 𝐜𝐨𝐧 𝐧𝐚𝐝𝐢𝐞 𝐬𝐮 𝐜𝐮𝐞𝐧𝐭𝐚. 𝐒𝐢 𝐬𝐞 𝐝𝐞𝐭𝐞𝐜𝐭𝐚 𝐦𝐚𝐥 𝐮𝐬𝐨, 𝐥𝐚 𝐜𝐮𝐞𝐧𝐭𝐚 𝐬𝐞 𝐛𝐥𝐨𝐪𝐮𝐞𝐚 𝐬𝐢𝐧 𝐠𝐚𝐫𝐚𝐧𝐭í𝐚 𝐧𝐢 𝐫𝐞𝐞𝐦𝐛𝐨𝐥𝐬𝐨.
+*🚫 El incumplimiento de estas reglas puede causar la pérdida del acceso sin derecho a reembolso.*
+`,
+
+pc:`
+
+*📽️ 𝘾𝘼𝙋𝘾𝙐𝙏 𝙋𝙍𝙊 📸*
+¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬
+📧: {correo}
+🔑: {contraseña}
+📅 𝐕𝐞𝐧𝐜𝐞: {vencimiento}
+*📱Dispositivo:* pc
+¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬
+*💻 ¿𝐐𝐮𝐢𝐞𝐫𝐞𝐬 𝐮𝐬𝐚𝐫 𝐞𝐧 𝐏𝐂?*
+1. 𝐏𝐫𝐢𝐦𝐞𝐫𝐨 𝐢𝐧𝐢𝐜𝐢𝐚 𝐬𝐞𝐬𝐢ó𝐧 𝐞𝐧 𝐥𝐚 𝐚𝐩𝐩 𝐝𝐞𝐥 *𝐜𝐞𝐥𝐮𝐥𝐚𝐫*
+2. 𝐋𝐮𝐞𝐠𝐨 𝐚𝐛𝐫𝐞 𝐂𝐚𝐩𝐂𝐮𝐭 𝐞𝐧 𝐭𝐮 𝐏𝐂.
+3. 𝐄𝐬𝐜𝐚𝐧𝐞𝐚 𝐞𝐥 *𝐜ó𝐝𝐢𝐠𝐨 𝐐𝐑* 𝐜𝐨𝐧 𝐭𝐮 𝐜𝐞𝐥𝐮𝐥𝐚𝐫 𝐩𝐚𝐫𝐚 𝐢𝐧𝐠𝐫𝐞𝐬𝐚𝐫.
+4. 𝐃𝐞𝐬𝐩𝐮é𝐬 𝐝𝐞 𝐢𝐧𝐠𝐫𝐞𝐬𝐚𝐫 𝐞𝐧 𝐥𝐚 𝐏𝐂, *𝐂𝐢𝐞𝐫𝐫𝐞 𝐜𝐞𝐬𝐢𝐨𝐧 𝐞𝐧 𝐥𝐚 𝐚𝐩𝐩 𝐝𝐞𝐥 𝐜𝐞𝐥𝐮𝐥𝐚𝐫,𝐧𝐨 𝐝𝐞𝐬𝐢𝐬𝐭𝐚𝐥𝐞 𝐥𝐚 𝐚𝐩𝐥𝐢𝐜𝐚𝐜𝐢𝐨𝐧* 𝐩𝐚𝐫𝐚 𝐞𝐯𝐢𝐭𝐚𝐫 𝐜𝐨𝐧𝐟𝐥𝐢𝐜𝐭𝐨𝐬 𝐝𝐞 𝐬𝐞𝐬𝐢ó𝐧.
+*🚨IMPORTANTE🚨*
+* 𝐍𝐨 𝐯𝐢𝐧𝐜𝐮𝐥𝐚𝐫 𝐜𝐨𝐧 𝐓𝐢𝐤𝐓𝐨𝐤. 
+* 𝐔𝐬𝐚𝐫 𝐬𝐨𝐥𝐨 𝐞𝐧 𝟏 𝐝𝐢𝐬𝐩𝐨𝐬𝐢𝐭𝐢𝐯𝐨.
+* 𝐍𝐨 𝐜𝐨𝐦𝐩𝐚𝐫𝐭𝐚𝐬 𝐜𝐨𝐧 𝐧𝐚𝐝𝐢𝐞 𝐬𝐮 𝐜𝐮𝐞𝐧𝐭𝐚. 𝐒𝐢 𝐬𝐞 𝐝𝐞𝐭𝐞𝐜𝐭𝐚 𝐦𝐚𝐥 𝐮𝐬𝐨, 𝐥𝐚 𝐜𝐮𝐞𝐧𝐭𝐚 𝐬𝐞 𝐛𝐥𝐨𝐪𝐮𝐞𝐚 𝐬𝐢𝐧 𝐠𝐚𝐫𝐚𝐧𝐭í𝐚 𝐧𝐢 𝐫𝐞𝐞𝐦𝐛𝐨𝐥𝐬𝐨.
+*🚫 El incumplimiento de estas reglas puede causar la pérdida del acceso sin derecho a reembolso.*
+`
+}
+
+},
+
+// OTROS
+
+{
+name:"RAKUTEN PLUS",
+fields:["correo","contraseña"],
+template:`
+
+*🌟𝐕𝐈𝐊𝐘 𝐑𝐀𝐊𝐔𝐓𝐄𝐍 𝐏𝐥𝐮𝐬🌟*
+¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬
+📧 : {correo}
+🔑 : {contraseña}
+*📅 𝐕𝐞𝐧𝐜𝐞:* 30 días
+*📱Dispositivo:* 1 
+¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬
+🚫 𝐍𝐨 𝐜𝐚𝐦𝐛𝐢𝐚𝐫 𝐝𝐚𝐭𝐨𝐬 𝐝𝐞 𝐥𝐚 𝐜𝐮𝐞𝐧𝐭𝐚.
+🚫 𝐄𝐯𝐢𝐭𝐚𝐫 𝐨𝐜𝐮𝐩𝐚𝐫 𝐦𝐚𝐬 𝐝𝐞 𝟏 𝐝𝐢𝐬𝐩𝐨𝐬𝐢𝐭𝐢𝐯𝐨.
+*💬 𝐂𝐮𝐚𝐥𝐪𝐮𝐢𝐞𝐫 𝐩𝐫𝐨𝐛𝐥𝐞𝐦𝐚 𝐦𝐞 𝐦𝐚𝐧𝐝𝐚𝐬 𝐜𝐚𝐩𝐭𝐮𝐫𝐚 𝐝𝐞 𝐩𝐚𝐧𝐭𝐚𝐥𝐥𝐚.*`
+},
+
+{
+name:"APPLE TV+",
+fields:["correo","contraseña"],
+template:`
+
+*🍎📺 APPLE TV+ 📺🍏*
+¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬
+📧 : {correo}
+🔑 : {contraseña}
+*📱Dispositivo:* 1
+*📆 𝐕𝐞𝐧𝐜𝐞:* 30 dias
+¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬
+📵 𝐍𝐨 𝐜𝐚𝐦𝐛𝐢𝐚𝐫 𝐝𝐚𝐭𝐨𝐬 𝐧𝐢 𝐜𝐨𝐧𝐭𝐫𝐚𝐬𝐞ñ𝐚. 
+🚫 𝐎𝐜𝐮𝐩𝐚𝐫 𝐞𝐧 𝐮𝐧 𝐬𝐨𝐥𝐨 𝐝𝐢𝐬𝐩𝐨𝐬𝐢𝐭𝐢𝐯𝐨 𝐩𝐚𝐫𝐚 𝐞𝐯𝐢𝐭𝐚𝐫 𝐩𝐫𝐨𝐛𝐥𝐞𝐦𝐚𝐬.`
+},
+
+// INVITACIONES
+
+{
+name:"YOUTUBE PREMIUM",
+fields:["correo"],
+template:`
+
+*💌 Te envié la invitación a tu correo.*
+¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬
+📧 {correo}
+1️⃣ Haz clic en *"Aceptar"*.
+2️⃣ Luego, selecciona *"Unirse"*.
+_🎉 ¡Listo! *YouTube Premium* debería activarse de inmediato._
+¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬
+📸 *Si después de unirte no ves YouTube Premium activo, envíame una captura de pantalla y con gusto te ayudaré.*
+⚠️ *La invitación expira en 24 horas,* así que acéptala cuanto antes.
+`
+},
+
+{
+name:"CANVA PRO",
+fields:["correo"],
+template:`
+
+*💌 𝐋𝐚 𝐢𝐧𝐯𝐢𝐭𝐚𝐜𝐢ó𝐧 𝐟𝐮𝐞 𝐞𝐧𝐯𝐢𝐚𝐝𝐚 𝐚𝐥 𝐜𝐨𝐫𝐫𝐞𝐨 𝐪𝐮𝐞 𝐦𝐞 𝐜𝐨𝐦𝐩𝐚𝐫𝐭𝐢𝐬𝐭𝐞.*
+¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬
+📧 {correo}
+𝟏️⃣ 𝐇𝐚𝐳 𝐜𝐥𝐢𝐜 𝐞𝐧 𝐞𝐥 𝐛𝐨𝐭ó𝐧 *"𝐀𝐜𝐞𝐩𝐭𝐚𝐫 𝐢𝐧𝐯𝐢𝐭𝐚𝐜𝐢ó𝐧"* 𝐝𝐞𝐬𝐝𝐞 𝐞𝐥 𝐜𝐨𝐫𝐫𝐞𝐨.
+𝟐️⃣ 𝐋𝐮𝐞𝐠𝐨 𝐬𝐞𝐥𝐞𝐜𝐜𝐢𝐨𝐧𝐚 *"𝐔𝐧𝐢𝐫𝐬𝐞 𝐚𝐥 𝐞𝐪𝐮𝐢𝐩𝐨"* 𝐩𝐚𝐫𝐚 𝐜𝐨𝐧𝐭𝐢𝐧𝐮𝐚𝐫.
+¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬
+*🎉 ¡Y listo! Canva Pro debería activarse de inmediato. 🥳*`
+},
+
+// ADULTOS
+
+{
+name:"PORNHUB PREMIUM",
+fields:["usuario","contraseña"],
+template:`
+
+*🔥𝐏𝐎𝐑𝐍𝐇𝐔𝐁 𝐏𝐑𝐄𝐌𝐈𝐔𝐌🔥*
+👤 : {usuario}
+🔑 : {contraseña}
+*📅 𝐕𝐞𝐧𝐜𝐞:* 30 días
+*📲 Uso:* 1 dispositivo
+🔗 𝐋𝐨𝐠𝐢𝐧: pornhub.com
+✅ *Recomendación:* No compartas el acceso para evitar bloqueos.
+`
+},
+
+{
+name:"BRAZZERS PREMIUM",
+fields:["usuario","contraseña"],
+template:`
+
+*🔞𝐁𝐑𝐀𝐙𝐙𝐄𝐑𝐒 𝐏𝐑𝐄𝐌𝐈𝐔𝐌🔥*
+👤 : {usuario}
+🔑 : {contraseña}
+*📅 𝐕𝐞𝐧𝐜𝐞:* 30 días
+*📲 Uso:* 1 dispositivo
+🔗 𝐋𝐨𝐠𝐢𝐧: brazzers.com
+✅ *Recomendación:* No compartas el acceso para evitar bloqueos.`
+},
+
+
+{
+name:"SPOTIFY PREMIUM",
+fields:["correo","contraseña"],
+template:`*🎵 𝐒𝐏𝐎𝐓𝐈𝐅𝐘 𝐏𝐑𝐄𝐌𝐈𝐔𝐌 🎧*
+¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬
+📧: {correo}
+🔑: {contraseña}
+📅: 30 días
+¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬
+🚫 𝐍𝐨 𝐜𝐚𝐦𝐛𝐢𝐚𝐫 𝐞𝐥 𝐜𝐨𝐫𝐫𝐞𝐨 𝐧𝐢 𝐥𝐚 𝐜𝐨𝐧𝐭𝐫𝐚𝐬𝐞ñ𝐚.
+🚫 𝐍𝐨 𝐜𝐨𝐦𝐩𝐚𝐫𝐭𝐢𝐫 𝐥𝐚 𝐜𝐮𝐞𝐧𝐭𝐚.
+_🔒Si se modifica el correo o contraseña se pierde la garantía y no habrá reclamo._
+
+💬 ꜱɪ ᴛɪᴇɴᴇꜱ ᴀʟɢúɴ ᴘʀᴏʙʟᴇᴍᴀ ᴄᴏɴ ʟᴀ ᴄᴜᴇɴᴛᴀ, ᴇꜱᴄʀíʙᴇᴍᴇ.`
+},
+
+{
+name:"TIDAL PREMIUM",
+fields:["correo","contraseña"],
+template:`*🎵 𝐓𝐈𝐃𝐀𝐋 𝐏𝐑𝐄𝐌𝐈𝐔𝐌 🎧*
+¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬
+📧: {correo}
+🔑: {contraseña}
+📅: 30 días
+¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬
+*⚠️ Recomendaciones:*
+🚫 𝐍𝐨 𝐜𝐚𝐦𝐛𝐢𝐚𝐫 𝐥𝐚 𝐜𝐨𝐧𝐭𝐫𝐚𝐬𝐞ñ𝐚 𝐧𝐢 𝐝𝐚𝐭𝐨𝐬 𝐝𝐞𝐥 𝐩𝐞𝐫𝐟𝐢𝐥.
+🚫 𝐄𝐯𝐢𝐭𝐚𝐫 𝐞𝐥 𝐮𝐬𝐨 𝐞𝐧 𝐦ú𝐥𝐭𝐢𝐩𝐥𝐞𝐬 𝐝𝐢𝐬𝐩𝐨𝐬𝐢𝐭𝐢𝐯𝐨𝐬 𝐚𝐥 𝐦𝐢𝐬𝐦𝐨 𝐭𝐢𝐞𝐦𝐩𝐨.
+
+💬 𝐏𝐚𝐫𝐚 𝐜𝐮𝐚𝐥𝐪𝐮𝐢𝐞𝐫 𝐩𝐫𝐨𝐛𝐥𝐞𝐦𝐚, 𝐜𝐨𝐧𝐭á𝐜𝐭𝐚𝐦𝐞 𝐝𝐢𝐫𝐞𝐜𝐭𝐚𝐦𝐞𝐧𝐭𝐞.`
+},
+
+{
+name:"DEEZER PREMIUM",
+fields:["correo","contraseña"],
+template:`*🎧 𝐃𝐄𝐄𝐙𝐄𝐑 𝐏𝐑𝐄𝐌𝐈𝐔𝐌 🔵*
+¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬
+📧: {correo}
+🔑: {contraseña}
+📅: 30 dias
+¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬¬
+🚫 𝐍𝐨 𝐜𝐚𝐦𝐛𝐢𝐚𝐫 𝐞𝐥 𝐜𝐨𝐫𝐫𝐞𝐨 𝐧𝐢 𝐦𝐨𝐝𝐢𝐟𝐢𝐜𝐚𝐫 𝐥𝐚 𝐜𝐨𝐧𝐭𝐫𝐚𝐬𝐞ñ𝐚.
+*_🔒 Si se modifica algún dato se pierde la garantía._*`
+},
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+]
+
+// CREAR CARDS
+
+const container=document.getElementById("services")
+
+services.forEach((s,i)=>{
+
+
+    
+let card=document.createElement("div")
+card.className="card "+s.name.replace(/\s|\+/g,"")
+
+card.innerHTML=`<h3>${s.name}</h3>`
+card.onclick=()=>openService(i)
+
+container.appendChild(card)
+
+})
+
+let currentService
+
+function openService(i){
+
+currentService=services[i]
+
+document.querySelector(".services").style.display="none"
+document.querySelector(".search-box").style.display="none"
+
+document.getElementById("formSection").classList.remove("hidden")
+
+document.getElementById("serviceTitle").innerText=currentService.name
+
+let form=""
+
+currentService.fields.forEach(f=>{
+
+if(f=="dispositivos"){
+
+form+=`
+<select id="${f}">
+<option value="1">1 dispositivo</option>
+<option value="2">2 dispositivos</option>
+<option value="3">3 dispositivos</option>
+</select>
+`
+
+}
+
+else if(f=="dispositivo"){
+
+form+=`
+<select id="${f}">
+<option value="PC">PC</option>
+<option value="Movil">Movil</option>
+</select>
+`
+
+}
+
+else{
+
+form+=`<input id="${f}" placeholder="${f}">`
+
+}
+
+})
+
+document.getElementById("dynamicForm").innerHTML=form
+
+}
+
+function generateText(){
+
+let text=""
+
+// si el servicio tiene templates por dispositivo
+if(currentService.templates){
+
+let device=document.getElementById("dispositivo").value.toLowerCase()
+
+text=currentService.templates[device]
+
+}else{
+
+text=currentService.template
+
+}
+
+// reemplazar campos
+currentService.fields.forEach(f=>{
+
+let val=document.getElementById(f).value
+
+let reg=new RegExp(`{${f}}`,"g")
+
+text=text.replace(reg,val)
+
+})
+
+document.getElementById("result").value=text
+
+}
+
+function copyText(){
+
+let text=document.getElementById("result")
+
+text.select()
+
+document.execCommand("copy")
+
+alert("Mensaje copiado")
+
+}
+
+function goBack(){
+
+location.reload()
+
+}
+
+document.getElementById("search").addEventListener("input",function(){
+
+let term=this.value.toLowerCase()
+
+document.querySelectorAll(".card").forEach(card=>{
+
+card.style.display=card.innerText.toLowerCase().includes(term)?"block":"none"
+
+})
+
+})
+
+
+function openCalculator(){
+window.location.href="conversor_bs_usd.html"
+}
+
+
+
+window.addEventListener("load",function(){
+
+setTimeout(()=>{
+
+document.getElementById("loadingScreen").style.display="none"
+
+},1500)
+
+})
+
+
+
+
+
